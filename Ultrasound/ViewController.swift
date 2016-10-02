78fbfe2a5de4eaaece3be2d654c30bcc4c4be3f5
Mark Chen <mark.j.chen@duke.edu>
2016-10-02 01:37:55 -0400

    Initial Commit

---
+++ Ultrasound/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Ultrasound
+//
+//  Created by Mark C on 10/2/16.
+//  Copyright © 2016 Duke University. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
