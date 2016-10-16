7bbdc19df82d070f3f0c92d8471e660b8322d9ec
Mark Chen <mark@mjc61.local>
2016-10-16 18:49:59 -0400

    Adding more screens

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 README for Ultrasound Classification iOS App
+
+To create multiple screens, see this: https://makeapppie.com/2014/07/01/swift-swift-using-segues-and-delegates-in-navigation-controllers-part-1-the-template/
---
+++ Ultrasound/ResultsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ResultsViewController.swift
+//  Ultrasound
+//
+//  Created by Mark Chen on 10/16/16.
+//  Copyright © 2016 Duke University. All rights reserved.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
